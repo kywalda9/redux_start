c34142a8549468bef3d7cf87d74dac1b3b3dabb9
Bohn <y2742@softwareag.com>
2020-04-24 01:51:31 +0200

    add thunk

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 
+import counterReducer from './store/reducers/counter';
+import resultReducer from './store/reducers/result';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import counterReducer from './store/reducers/counter';
-import resultReducer from './store/reducers/result';
-
 
 const rootReducer = combineReducers({
     ctr: counterReducer,
@@ -18,17 +18,17 @@ const rootReducer = combineReducers({
 const logger = store => {
     return next => {
         return action => {
-           console.log('[Middleware] Dispatching', action);
-           const result = next(action);
-           console.log('[Middleware] next state', store.getState());
-           return result;
-           
+            console.log('[Middleware] Dispatching', action);
+            const result = next(action);
+            console.log('[Middleware] next state', store.getState());
+            return result;
         }
     }
-}
-const  composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+};
 
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger)));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger, thunk)));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
